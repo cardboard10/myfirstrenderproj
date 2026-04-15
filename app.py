1ed100354c750272f7d09f6d9af980cb4a8d9ac2
cardboard10 <104028962+cardboard10@users.noreply.github.com>
2026-04-15 10:37:12 -0400

    Rename hello function to index and update response

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>Hello! This is Landon's Python Server.</h1><p>No HTML files here, just pure Python logic.</p>"
+def index():
+    return "SUCCESS: Python is alive!"
 
 if __name__ == "__main__":
     app.run()
